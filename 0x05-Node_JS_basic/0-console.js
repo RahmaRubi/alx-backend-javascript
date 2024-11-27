46e921a7cb1587768fd79f1589fd3dbb312a139b
root <root@DESKTOP-OB6IUIJ>
2024-11-27 14:09:35 +0200

    first node

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+// 0-console.js
+function displayMessage(message) {
+    console.log(message); // Prints the message to STDOUT
+  }
+  
+  module.exports = displayMessage;
+  
---
+++ 0x05-Node_JS_basic/0-main.js
@@ -0,0 +1,5 @@
+// 0-main.js
+const displayMessage = require('./0-console'); // Import the function
+
+displayMessage("Hello NodeJS!"); // Call the function with a sample message
+
